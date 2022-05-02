f7a51b2a9eb3b1c94b054f599dab98e667b3ac16
Ma-y-ank <mayank.agarwal@srnext.in>
2022-05-02 07:53:49 +0530

    Trainer and trainee association added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
+
+  has_many :trainees,
+    foreign_key: :trainer_id,
+    class_name: :User
+
+  belongs_to :trainer, 
+    primary_key: :id,
+    foreign_key: :trainer_id,
+    class_name: :User,
+    optional: true
 end
